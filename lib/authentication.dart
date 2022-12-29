6b5d29000dab0c7d98fc8709fbbd1fedf1c09c05
p-olszewski <przemo1297@gmail.com>
2022-12-29 14:54:09 +0100

    Prepare authentication file

---
+++ lib/authentication.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Authentication extends StatefulWidget {
+  const Authentication({super.key});
+
+  @override
+  State<Authentication> createState() => _AuthenticationState();
+}
+
+class _AuthenticationState extends State<Authentication> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
