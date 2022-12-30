a8ec985e2dd45f53f937796260a4b028a8c101df
p-olszewski <przemo1297@gmail.com>
2022-12-30 11:38:30 +0100

    Add WidgetsFlutterBinding

--- lib/ui/main.dart
+++ lib/ui/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import '../firebase_options.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
