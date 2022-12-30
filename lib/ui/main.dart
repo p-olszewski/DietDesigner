7a4c3dbf8e8ab45c1f4c796b3258ab884eee3e15
p-olszewski <przemo1297@gmail.com>
2022-12-30 11:36:25 +0100

    Create ui folder, move ui files

--- lib/ui/main.dart
+++ lib/ui/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import '../firebase_options.dart';
 
 Future<void> main() async {
   await Firebase.initializeApp(
