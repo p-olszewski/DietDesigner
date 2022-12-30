a80ca2307629e4c9436bcfef5041ce9590f1132f
p-olszewski <przemo1297@gmail.com>
2022-12-30 12:24:36 +0100

    Main.dart next refactor

--- lib/ui/main.dart
+++ lib/ui/main.dart
@@ -5,9 +5,7 @@ import '../firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
