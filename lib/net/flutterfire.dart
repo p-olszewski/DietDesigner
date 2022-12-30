0de3bbd7db272eb68dda2972b6bdc72b16a447fd
p-olszewski <przemo1297@gmail.com>
2022-12-30 12:31:50 +0100

    Add signUp method

--- lib/net/flutterfire.dart
+++ lib/net/flutterfire.dart
@@ -11,3 +11,21 @@ Future<bool> signIn(String email, String password) async {
     return false;
   }
 }
+
+Future<bool> signUp(String email, String password) async {
+  try {
+    await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email, password: password);
+    return true;
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'weak-password') {
+      debugPrint('The password provided is too weak.');
+    } else if (e.code == 'email-already-in-use') {
+      debugPrint('The account already exists for that email.');
+    }
+    return false;
+  } catch (e) {
+    debugPrint(e.toString());
+    return false;
+  }
+}
