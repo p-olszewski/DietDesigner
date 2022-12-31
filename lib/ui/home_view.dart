823c68dbef424f6b137952404828a2c0f1559c5e
p-olszewski <przemo1297@gmail.com>
2022-12-31 01:17:16 +0100

    Add toast to log out action

--- lib/ui/home_view.dart
+++ lib/ui/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:diet_designer/net/flutterfire.dart';
 import 'package:diet_designer/ui/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -22,12 +23,13 @@ class _HomeViewState extends State<HomeView> {
               bool shouldRedirect = await signOut();
               if (shouldRedirect) {
                 if (!mounted) return;
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const LoginPage(),
-                  ),
-                );
+                Fluttertoast.showToast(msg: "Logged out");
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(
+                //     builder: (context) => const LoginPage(),
+                //   ),
+                // );
               }
             },
             icon: const Icon(Icons.logout),
