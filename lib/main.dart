229a8bd02ad1c3aef680c37b3b159c3541322d17
p-olszewski <przemo1297@gmail.com>
2023-03-04 19:51:21 +0100

    Load .env while program start

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:diet_designer/ui/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
