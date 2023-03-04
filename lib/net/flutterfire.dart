8760b13234b9d9a4c04c661a7ef3726c77e2d2ec
p-olszewski <przemo1297@gmail.com>
2023-03-04 20:49:53 +0100

    SignIn method user-not-found and wrong-password validation

--- lib/net/flutterfire.dart
+++ lib/net/flutterfire.dart
@@ -6,6 +6,13 @@ Future<bool> signIn(String email, String password) async {
     await FirebaseAuth.instance
         .signInWithEmailAndPassword(email: email, password: password);
     return true;
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'user-not-found') {
+      Fluttertoast.showToast(msg: 'This email address is not registered.');
+    } else if (e.code == 'wrong-password') {
+      Fluttertoast.showToast(msg: 'Wrong password.');
+    }
+    return false;
   } catch (e) {
     Fluttertoast.showToast(msg: e.toString());
     return false;
