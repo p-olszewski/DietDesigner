380e31bb65376819a6e82bc56b2606eba1e77468
p-olszewski <przemo1297@gmail.com>
2023-03-12 13:29:50 +0100

    merge login and registration to one widget

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -2,7 +2,7 @@ import 'package:diet_designer/services/flutterfire.dart';
 import 'package:diet_designer/calculator/calculator.dart';
 import 'package:diet_designer/home/home_view.dart';
 import 'package:diet_designer/registration/registration_page.dart';
-import 'package:diet_designer/shared/custom_textformfield.dart';
+import 'package:diet_designer/shared/shared.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
@@ -14,9 +14,10 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final TextEditingController _emailField = TextEditingController();
-  final TextEditingController _passwordField = TextEditingController();
-  final TextEditingController _repeatedPasswordField = TextEditingController();
+  final TextEditingController _emailFieldController = TextEditingController();
+  final TextEditingController _passwordFieldController = TextEditingController();
+  final TextEditingController _repeatedPasswordFieldController = TextEditingController();
+  bool _isLoginPage = true;
 
   @override
   Widget build(BuildContext context) {
@@ -32,20 +33,34 @@ class _LoginPageState extends State<LoginPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              "Sign in to",
-              style: TextStyle(color: Colors.white, fontSize: 32, fontWeight: FontWeight.w600),
-            ),
-            const Text(
-              "DietDesigner",
-              style: TextStyle(color: Colors.white, fontSize: 48, fontWeight: FontWeight.w100),
+            _isLoginPage
+                ? Column(
+                    children: const [
+                      Text(
+                        "Sign in to",
+                        style: TextStyle(color: Colors.white, fontSize: 32, fontWeight: FontWeight.w600),
+                      ),
+                      Text(
+                        "DietDesigner",
+                        style: TextStyle(color: Colors.white, fontSize: 48, fontWeight: FontWeight.w100),
+                      ),
+                    ],
+                  )
+                : const Text(
+                    "Create Account",
+                    style: TextStyle(color: Colors.white, fontSize: 32, fontWeight: FontWeight.w600),
+                  ),
+            SizedBox(height: screenHeight / 15),
+            CustomTextFormField(controller: _emailFieldController, labelText: "Email", hintText: "youremail@email.com", obscureText: false),
+            SizedBox(height: screenHeight / 100),
+            CustomTextFormField(controller: _passwordFieldController, labelText: "Password", hintText: "password", obscureText: true),
+            SizedBox(height: screenHeight / 100),
+            Visibility(
+              visible: !_isLoginPage,
+              child: CustomTextFormField(
+                  controller: _repeatedPasswordFieldController, labelText: "Repeat password", hintText: "password", obscureText: true),
             ),
             SizedBox(height: screenHeight / 15),
-            CustomTextFormField(controller: _emailField, labelText: "Email", hintText: "youremail@email.com", obscureText: false),
-            SizedBox(height: screenHeight / 100),
-            CustomTextFormField(controller: _passwordField, labelText: "Password", hintText: "password", obscureText: true),
-            SizedBox(height: screenHeight / 20),
-            SizedBox(height: screenHeight / 100),
             Container(
               width: widgetWidth / 1.3,
               height: 45,
@@ -55,9 +70,11 @@ class _LoginPageState extends State<LoginPage> {
               ),
               child: MaterialButton(
                 onPressed: () async {
-                  bool shouldRedirect = await signIn(_emailField.text, _passwordField.text);
+                  bool shouldRedirect = _isLoginPage
+                      ? await signIn(_emailFieldController.text, _passwordFieldController.text)
+                      : await signUp(_emailFieldController.text, _passwordFieldController.text);
                   if (shouldRedirect) {
-                    Fluttertoast.showToast(msg: "Logged in");
+                    Fluttertoast.showToast(msg: _isLoginPage ? "Logged in" : "Account created");
                     bool calculatedCalories = await checkUserCalculatedCalories();
                     if (!mounted) return;
                     if (calculatedCalories) {
@@ -77,25 +94,20 @@ class _LoginPageState extends State<LoginPage> {
                     }
                   }
                 },
-                child: const Text("Login"),
+                child: Text(_isLoginPage ? "Login" : "Register and login"),
               ),
             ),
             MaterialButton(
-              onPressed: () async {
-                if (!mounted) return;
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const RegistrationPage(),
-                  ),
-                );
-              },
-              child: const Text(
-                "or go to registration page",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w400,
-                ),
+              onPressed: () => setState(() {
+                _isLoginPage = !_isLoginPage;
+                _emailFieldController.clear();
+                _passwordFieldController.clear();
+                _repeatedPasswordFieldController.clear();
+                FocusScope.of(context).unfocus();
+              }),
+              child: Text(
+                _isLoginPage ? "go to registration page" : "go back to the login page",
+                style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w400),
               ),
             ),
           ],
