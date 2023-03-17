1ae2744ff764796df90964b2b2be5c8e341ac8e4
p-olszewski <przemo1297@gmail.com>
2023-03-17 10:24:20 +0100

    create logo file

---
+++ lib/shared/logo.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Logo extends StatelessWidget {
+  const Logo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "DietDesigner",
+      style: TextStyle(color: Colors.white, fontSize: 64, fontFamily: GoogleFonts.indieFlower().fontFamily),
+    );
+  }
+}
