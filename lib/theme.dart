5c998322d95e173c422255615d27780abe87b490
p-olszewski <przemo1297@gmail.com>
2023-03-17 09:12:34 +0100

    create theme file

---
+++ lib/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+var appTheme = ThemeData(
+  fontFamily: GoogleFonts.poppins().fontFamily,
+  primarySwatch: Colors.green,
+);
