dce39c2ea6ffb1e24b5d0b6e8b70df858ba09317
p-olszewski <przemo1297@gmail.com>
2023-03-26 22:15:39 +0200

    extract calculator inputs

--- lib/calculator/calculator.dart
+++ lib/calculator/calculator.dart
@@ -21,131 +21,128 @@ class _CalculatorState extends State<Calculator> {
 
   @override
   Widget build(BuildContext context) {
+    Row activitySlider = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Activity:"),
+        SizedBox(
+          width: 250,
+          child: Slider(
+            value: _activity,
+            onChanged: (value) => setState(() => _activity = value),
+            divisions: 4,
+            min: 1,
+            max: 5,
+            label: _activity.round().toString(),
+          ),
+        ),
+      ],
+    );
+
+    Row genderRow = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Gender:"),
+        Radio(
+          value: 'woman',
+          groupValue: _gender,
+          onChanged: (value) => setState(() => _gender = value!),
+        ),
+        const Text("woman"),
+        Radio(
+          value: 'man',
+          groupValue: _gender,
+          onChanged: (value) => setState(() => _gender = value!),
+        ),
+        const Text("man"),
+      ],
+    );
+
+    Row targetRow = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Target:"),
+        Radio(
+          value: 'cut',
+          groupValue: _target,
+          onChanged: (value) => setState(() => _target = value!),
+        ),
+        const Text("cut"),
+        Radio(
+          value: 'stay',
+          groupValue: _target,
+          onChanged: (value) => setState(() => _target = value!),
+        ),
+        const Text("stay"),
+        Radio(
+          value: 'gain',
+          groupValue: _target,
+          onChanged: (value) => setState(() => _target = value!),
+        ),
+        const Text("gain"),
+      ],
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Calculator"),
         centerTitle: true,
       ),
       body: Center(
-        child: Column(
-          children: [
-            const SizedBox(height: 40),
-            const Text("Enter your data:"),
-            const SizedBox(height: 40),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("Gender:"),
-                Radio(
-                  value: 'woman',
-                  groupValue: _gender,
-                  onChanged: (value) => setState(() => _gender = value!),
-                ),
-                const Text("woman"),
-                Radio(
-                  value: 'man',
-                  groupValue: _gender,
-                  onChanged: (value) => setState(() => _gender = value!),
-                ),
-                const Text("man"),
-              ],
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("Age:"),
-                const SizedBox(width: 15),
-                SizedBox(
-                  width: 200,
-                  child: TextField(
-                    controller: _ageFieldController,
-                    decoration: const InputDecoration(border: UnderlineInputBorder()),
-                  ),
-                )
-              ],
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("Height:"),
-                const SizedBox(width: 15),
-                SizedBox(
-                  width: 200,
-                  child: TextField(
-                    controller: _heightFieldController,
-                    decoration: const InputDecoration(border: UnderlineInputBorder()),
-                  ),
-                )
-              ],
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("Weight:"),
-                const SizedBox(width: 15),
-                SizedBox(
-                  width: 200,
-                  child: TextField(
-                    controller: _weightFieldController,
-                    decoration: const InputDecoration(border: UnderlineInputBorder()),
-                  ),
-                )
-              ],
-            ),
-            const SizedBox(height: 30),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("Activity:"),
-                SizedBox(
-                  width: 250,
-                  child: Slider(
-                    value: _activity,
-                    onChanged: (value) => setState(() => _activity = value),
-                    divisions: 4,
-                    min: 1,
-                    max: 5,
-                    label: _activity.round().toString(),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("Target:"),
-                Radio(
-                  value: 'cut',
-                  groupValue: _target,
-                  onChanged: (value) => setState(() => _target = value!),
-                ),
-                const Text("cut"),
-                Radio(
-                  value: 'stay',
-                  groupValue: _target,
-                  onChanged: (value) => setState(() => _target = value!),
-                ),
-                const Text("stay"),
-                Radio(
-                  value: 'gain',
-                  groupValue: _target,
-                  onChanged: (value) => setState(() => _target = value!),
-                ),
-                const Text("gain"),
-              ],
-            ),
-            const SizedBox(height: 40),
-            FilledButton(
-              onPressed: () {},
-              child: const Text("Save"),
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Text("Enter your data:"),
+              const SizedBox(height: 40),
+              genderRow,
+              const SizedBox(height: 20),
+              TextInput(controller: _ageFieldController, labelText: "Age:"),
+              const SizedBox(height: 20),
+              TextInput(controller: _heightFieldController, labelText: "Height:"),
+              const SizedBox(height: 20),
+              TextInput(controller: _weightFieldController, labelText: "Weight:"),
+              const SizedBox(height: 30),
+              activitySlider,
+              const SizedBox(height: 20),
+              targetRow,
+              const SizedBox(height: 40),
+              FilledButton(
+                onPressed: () {},
+                child: const Text("Save"),
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class TextInput extends StatelessWidget {
+  const TextInput({
+    super.key,
+    required this.controller,
+    required this.labelText,
+  });
+
+  final TextEditingController controller;
+  final String labelText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(labelText),
+        const SizedBox(width: 15),
+        SizedBox(
+          width: 200,
+          child: TextField(
+            controller: controller,
+            decoration: const InputDecoration(border: UnderlineInputBorder()),
+          ),
+        )
+      ],
     );
   }
 }
