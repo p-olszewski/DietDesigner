0519f879dad45142445a96453bb5e1d51a1858a5
p-olszewski <przemo1297@gmail.com>
2023-03-26 13:21:19 +0200

    switch to material3

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 var appTheme = ThemeData(
+  useMaterial3: true,
+  colorSchemeSeed: Colors.green,
   fontFamily: GoogleFonts.poppins().fontFamily,
-  colorScheme: ColorScheme.fromSwatch(
-    primarySwatch: Colors.green,
-    accentColor: Colors.yellow,
-  ),
+  // colorScheme: ColorScheme.fromSwatch(
+  //   primarySwatch: Colors.green,
+  //   accentColor: Colors.yellow,
+  // ),
 );
