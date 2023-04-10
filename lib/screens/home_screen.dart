17f3f3b5e398c9855d8d149a58deffae883b002a
p-olszewski <przemo1297@gmail.com>
2023-04-10 22:21:50 +0200

    add text above the meal card, implement sample list of meals cards in the home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:diet_designer/services/auth.dart';
 import 'package:diet_designer/screens/login_screen.dart';
 import 'package:diet_designer/shared/shared.dart';
+import 'package:diet_designer/widgets/meal_card.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -40,8 +41,36 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
       drawer: const DrawerMenu(),
-      body: const Center(
-        child: Text("Home page"),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Text(
+                "Your meal plan today: ",
+                style: TextStyle(fontSize: 18),
+              ),
+              ListView.builder(
+                physics: const ScrollPhysics(),
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return MealCard(
+                    meal: {
+                      'title': 'Meal ${index + 1}',
+                      'calories': '523',
+                      'protein': '32',
+                      'fat': '15',
+                      'carbs': '74',
+                      'image': 'https://spoonacular.com/recipeImages/658418-312x231.jpg',
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
       ),
       bottomNavigationBar: const BottomNavbarMenu(),
     );
--- lib/widgets/meal_card.dart
+++ lib/widgets/meal_card.dart
@@ -11,33 +11,40 @@ class MealCard extends StatelessWidget {
 /* card to display each meal */
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        contentPadding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-        leading: Container(
-          width: 50,
-          height: 60,
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.2),
-                spreadRadius: 1,
-                blurRadius: 6,
-                offset: const Offset(1, 1),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 20),
+        Text("${meal['title']} - ${meal['calories']} kcal, ${meal['protein']}g/${meal['fat']}g/${meal['carbs']}g"),
+        Card(
+          child: ListTile(
+            contentPadding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            leading: Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    spreadRadius: 1,
+                    blurRadius: 6,
+                    offset: const Offset(1, 1),
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(15.0),
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(meal['image']),
+                ),
               ),
-            ],
-            borderRadius: BorderRadius.circular(15.0),
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: NetworkImage(meal['image']),
+            ),
+            title: Text(meal['title']),
+            subtitle: Text(
+              '${meal['calories']} kcal\n${meal['protein']} protein, ${meal['fat']} fat, ${meal['carbs']} carbs',
             ),
           ),
         ),
-        title: Text(meal['title']),
-        subtitle: Text(
-          '${meal['calories']} kcal\n${meal['protein']} protein, ${meal['fat']} fat, ${meal['carbs']} carbs',
-        ),
-      ),
+      ],
     );
   }
 }
