d0b2250155de9d230ca00bbccfbd30bcdcbf0c8e
p-olszewski <przemo1297@gmail.com>
2023-04-10 22:03:34 +0200

    create MealCard widget

---
+++ lib/widgets/meal_card.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class MealCard extends StatelessWidget {
+  const MealCard({
+    super.key,
+    required this.meal,
+  });
+
+  final dynamic meal;
+
+/* card to display each meal */
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        contentPadding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        leading: Container(
+          width: 50,
+          height: 60,
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                spreadRadius: 1,
+                blurRadius: 6,
+                offset: const Offset(1, 1),
+              ),
+            ],
+            borderRadius: BorderRadius.circular(15.0),
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(meal['image']),
+            ),
+          ),
+        ),
+        title: Text(meal['title']),
+        subtitle: Text(
+          '${meal['calories']} kcal\n${meal['protein']} protein, ${meal['fat']} fat, ${meal['carbs']} carbs',
+        ),
+      ),
+    );
+  }
+}
