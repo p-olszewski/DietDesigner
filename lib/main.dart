29a88c1db8c400056cc8d07351d4b11041955479
p-olszewski <przemo1297@gmail.com>
2023-04-11 12:19:36 +0200

    change app logo icon to the modern dinner, add icon to web app

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Diet Designer',
+      title: 'DietDesigner',
       theme: appTheme,
       initialRoute: '/login', // temporary
       routes: appRoutes,
