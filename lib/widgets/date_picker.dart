154317b9b6e37d435191bdb9ca1aa2d2329a67a9
p-olszewski <przemo1297@gmail.com>
2023-04-21 12:25:05 +0200

    implement date picker

--- lib/widgets/date_picker.dart
+++ lib/widgets/date_picker.dart
@@ -1,4 +1,6 @@
+import 'package:diet_designer/providers/date_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class DatePicker extends StatelessWidget {
   const DatePicker({
@@ -11,9 +13,31 @@ class DatePicker extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         IconButton(onPressed: () {}, icon: const Icon(Icons.navigate_before)),
-        TextButton(onPressed: () {}, child: const Text('01.01.2023')),
+        TextButton(
+            onPressed: () async {
+              DateTime? newDate = await showDatePicker(
+                context: context,
+                initialDate: context.read<DateProvider>().date,
+                firstDate: DateTime(1900),
+                lastDate: DateTime(2100),
+              );
+              if (newDate == null) return;
+              context.read<DateProvider>().setDate(newDate);
+            },
+            child: const DateButton()),
         IconButton(onPressed: () {}, icon: const Icon(Icons.navigate_next))
       ],
     );
+  }
+}
+
+class DateButton extends StatelessWidget {
+  const DateButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(context.watch<DateProvider>().formattedDate);
   }
 }
