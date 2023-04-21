df6c529085408c02bf5f33f82bc2119d57e0cf1e
p-olszewski <przemo1297@gmail.com>
2023-04-21 12:01:30 +0200

    change setDate to use formatted date

--- lib/providers/date_provider.dart
+++ lib/providers/date_provider.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class DateProvider with ChangeNotifier {
   String _date = '';
 
   String get date => _date;
 
-  void setDate(String date) {
-    _date = date;
+  void setDate(DateTime date) {
+    final String formattedDate = DateFormat('dd.MM.yyyy').format(date);
+    _date = formattedDate;
     notifyListeners();
   }
+
+  String getCurrentDate() => DateFormat('dd.MM.yyyy').format(DateTime.now());
 }
