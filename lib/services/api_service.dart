b2ac1643a08fc99e7844a033e9e8e872edc34e45
p-olszewski <przemo1297@gmail.com>
2023-04-23 20:46:40 +0200

    change api to call 2 request - for breakfast and for the rest of meals

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -16,41 +16,67 @@ class APIService {
 
   Future<List<Meal>?> fetchMeals(User user) async {
     Map<String, String> headers = {HttpHeaders.contentTypeHeader: 'application/json', 'x-api-key': apiKey};
-    final Map<String, String> parameters = _getPersonalizedParameters(user);
-    Uri uri = Uri.https(_baseUrl, _path, parameters);
+
+    final Map<String, String> breakfastParameters = _getPersonalizedParameters(user, mealsNumber: 1, mealType: 'breakfast');
+    Uri breakfastUri = Uri.https(_baseUrl, _path, breakfastParameters);
+
+    final Map<String, String> restOfMealsParameters = _getPersonalizedParameters(user, mealsNumber: user.mealsNumber! - 1);
+    Uri restOfMealsUri = Uri.https(_baseUrl, _path, restOfMealsParameters);
 
     try {
-      final response = await http.get(uri, headers: headers);
-      if (response.statusCode == 200) {
-        var data = jsonDecode(response.body);
-        var responseResults = data['results'];
+      final breakfastResponse = await http.get(breakfastUri, headers: headers);
+      final restOfMealsResponse = await http.get(restOfMealsUri, headers: headers);
+      if (breakfastResponse.statusCode == 200 && restOfMealsResponse.statusCode == 200) {
         List<Meal> meals = [];
-        for (var meal in responseResults) {
+        var breakfastData = jsonDecode(breakfastResponse.body);
+        var restOfMealsData = jsonDecode(restOfMealsResponse.body);
+        var breakfastResults = breakfastData['results'];
+        var restOfMealsResults = restOfMealsData['results'];
+
+        meals.add(Meal.fromJson(breakfastResults[0]));
+        for (var meal in restOfMealsResults) {
           meals.add(Meal.fromJson(meal));
         }
         return meals;
       } else {
-        throw Exception('Failed to load data: ${response.statusCode}');
+        throw Exception('Failed to load data: ${restOfMealsResponse.statusCode}');
       }
     } catch (e) {
       throw Exception('Failed to load data: $e');
     }
   }
 
-  Map<String, String> _getPersonalizedParameters(User user) {
-    int mealsNumber = user.mealsNumber!;
-    int kcal = (user.calories! / mealsNumber).round();
-    int protein = (user.proteins! / mealsNumber).round();
+  //TODO meal type distinction and validate meals number if user has a lot of calories (max meal kcal is less than 1000 kcal)
+  Map<String, String> _getPersonalizedParameters(User user, {required int mealsNumber, String mealType = ''}) {
+    final kcal = (user.calories! / user.mealsNumber!).round();
+    final protein = (user.proteins! / user.mealsNumber!).round();
+    Map<String, String> parameters;
+    switch (mealType) {
+      case 'breakfast':
+        parameters = {
+          'minCalories': (kcal - 150).toString(),
+          'maxCalories': (kcal + 150).toString(),
+          'offset': Random().nextInt(30).toString(),
+          'type': mealType,
+        };
+        break;
+      default:
+        parameters = {
+          'minCalories': (kcal - 50).toString(),
+          'maxCalories': (kcal + 50).toString(),
+          'minProtein': (protein - 10).toString(),
+          'maxProtein': (protein + 10).toString(),
+          'offset': Random().nextInt(50).toString(),
+        };
+        break;
+    }
+
     return {
-      'minCalories': (kcal - 50).toString(),
-      'maxCalories': (kcal + 50).toString(),
-      'minProtein': (protein - 10).toString(),
-      'maxProtein': (protein + 10).toString(),
       'number': mealsNumber.toString(),
-      'offset': Random().nextInt(50).toString(),
       'addRecipeInformation': true.toString(),
       'addRecipeNutrition': true.toString(),
       'sort': 'random',
+      ...parameters
     };
   }
 }
