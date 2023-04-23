b4e519fbe7c519a200a37ff14ebbbce38829620d
p-olszewski <przemo1297@gmail.com>
2023-04-23 21:13:44 +0200

    api service refactor

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'dart:math';
 import 'package:diet_designer/models/meal.dart';
 import 'package:diet_designer/models/user.dart';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -15,38 +16,31 @@ class APIService {
   final String apiKey = dotenv.env['SPOONACULAR_API_KEY']!;
 
   Future<List<Meal>?> fetchMeals(User user) async {
-    Map<String, String> headers = {HttpHeaders.contentTypeHeader: 'application/json', 'x-api-key': apiKey};
-
-    final Map<String, String> breakfastParameters = _getPersonalizedParameters(user, mealsNumber: 1, mealType: 'breakfast');
-    Uri breakfastUri = Uri.https(_baseUrl, _path, breakfastParameters);
-
-    final Map<String, String> restOfMealsParameters = _getPersonalizedParameters(user, mealsNumber: user.mealsNumber! - 1);
-    Uri restOfMealsUri = Uri.https(_baseUrl, _path, restOfMealsParameters);
-
     try {
-      final breakfastResponse = await http.get(breakfastUri, headers: headers);
-      final restOfMealsResponse = await http.get(restOfMealsUri, headers: headers);
-      if (breakfastResponse.statusCode == 200 && restOfMealsResponse.statusCode == 200) {
-        List<Meal> meals = [];
-        var breakfastData = jsonDecode(breakfastResponse.body);
-        var restOfMealsData = jsonDecode(restOfMealsResponse.body);
-        var breakfastResults = breakfastData['results'];
-        var restOfMealsResults = restOfMealsData['results'];
+      Map<String, String> headers = {HttpHeaders.contentTypeHeader: 'application/json', 'x-api-key': apiKey};
+      List<Meal> meals = [];
 
-        meals.add(Meal.fromJson(breakfastResults[0]));
-        for (var meal in restOfMealsResults) {
-          meals.add(Meal.fromJson(meal));
-        }
-        return meals;
-      } else {
-        throw Exception('Failed to load data: ${restOfMealsResponse.statusCode}');
+      final breakfastParameters = _getPersonalizedParameters(user, mealsNumber: 1, mealType: 'breakfast');
+      final restOfMealsParameters = _getPersonalizedParameters(user, mealsNumber: user.mealsNumber! - 1);
+
+      final breakfastResponse = await _makeRequest(_baseUrl, _path, breakfastParameters, headers);
+      final restOfMealsResponse = await _makeRequest(_baseUrl, _path, restOfMealsParameters, headers);
+
+      var breakfastResults = breakfastResponse['results'];
+      var restOfMealsResults = restOfMealsResponse['results'];
+
+      meals.add(Meal.fromJson(breakfastResults[0]));
+      for (var meal in restOfMealsResults) {
+        meals.add(Meal.fromJson(meal));
       }
+
+      return meals;
     } catch (e) {
+      debugPrint(e.toString());
       throw Exception('Failed to load data: $e');
     }
   }
 
-  //TODO meal type distinction and validate meals number if user has a lot of calories (max meal kcal is less than 1000 kcal)
   Map<String, String> _getPersonalizedParameters(User user, {required int mealsNumber, String mealType = ''}) {
     final kcal = (user.calories! / user.mealsNumber!).round();
     final protein = (user.proteins! / user.mealsNumber!).round();
@@ -56,7 +50,7 @@ class APIService {
         parameters = {
           'minCalories': (kcal - 150).toString(),
           'maxCalories': (kcal + 150).toString(),
-          'offset': Random().nextInt(30).toString(),
+          'offset': Random().nextInt(50).toString(),
           'type': mealType,
         };
         break;
@@ -78,5 +72,21 @@ class APIService {
       'sort': 'random',
       ...parameters
     };
+  }
+
+  Future<Map<String, dynamic>> _makeRequest(
+    String baseUrl,
+    String path,
+    Map<String, String> parameters,
+    Map<String, String> headers,
+  ) async {
+    Uri uri = Uri.https(baseUrl, path, parameters);
+    final response = await http.get(uri, headers: headers);
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load data: ${response.statusCode}');
+    }
   }
 }
