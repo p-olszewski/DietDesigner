f0203b8f248d86bd14bf43e38918bdf804626876
p-olszewski <przemo1297@gmail.com>
2023-04-24 23:38:14 +0200

    display nutrients in containers

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -93,6 +93,35 @@ class MealDetailsData extends StatelessWidget {
 
   final Meal meal;
 
+  Container _buildNutritionInfoContainer(String value, String label) {
+    return Container(
+      width: 80,
+      height: 60,
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            value,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          Text(
+            label,
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 14),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return DraggableScrollableSheet(
@@ -131,9 +160,19 @@ class MealDetailsData extends StatelessWidget {
                         meal.title,
                         style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                       ),
+                      const SizedBox(height: 24),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          _buildNutritionInfoContainer('${meal.calories.round()}', 'kcal'),
+                          _buildNutritionInfoContainer('${meal.proteins.round()}g', 'proteins'),
+                          _buildNutritionInfoContainer('${meal.fats.round()}g', 'fats'),
+                          _buildNutritionInfoContainer('${meal.carbs.round()}g', 'carbs'),
+                        ],
+                      ),
                       const SizedBox(height: 16),
                       Text(
-                          'Calories: ${meal.calories.round()} kcal\nProteins: ${meal.proteins.round()}g\nFats: ${meal.fats.round()}g\nCarbs: ${meal.carbs.round()}g\nReady in: ${meal.readyInMinutes} min\nServings: ${meal.servings}\nPrice per serving: ${meal.pricePerServing}\$'),
+                          'Ready in: ${meal.readyInMinutes} min\nServings: ${meal.servings}\nPrice per serving: ${meal.pricePerServing}\$'),
                       const SizedBox(height: 16),
                       const Text(
                         "Dish types:",
