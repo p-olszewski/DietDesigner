338d831ef9c3b2811871b74d74b7560bf1f26d3f
p-olszewski <przemo1297@gmail.com>
2023-04-24 09:28:56 +0200

    display all of meal data inside details screen

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -40,21 +40,32 @@ class MealDetailsScreen extends StatelessWidget {
                     ),
                     const SizedBox(height: 16),
                     Text(
-                      'Calories: ${meal.calories.round()} kcal\nProteins: ${meal.proteins.round()}g\nFats: ${meal.fats.round()}g\nCarbs: ${meal.carbs.round()}g',
-                      style: const TextStyle(fontSize: 16),
+                        'Calories: ${meal.calories.round()} kcal\nProteins: ${meal.proteins.round()}g\nFats: ${meal.fats.round()}g\nCarbs: ${meal.carbs.round()}g\nReady in: ${meal.readyInMinutes} min\nServings: ${meal.servings}\nPrice per serving: ${meal.pricePerServing}\$'),
+                    const SizedBox(height: 16),
+                    const Text(
+                      "Dish types:",
+                      style: TextStyle(fontWeight: FontWeight.bold),
                     ),
+                    ...meal.dishTypes!.map((dishType) {
+                      return Text(dishType);
+                    }).toList(),
+                    const SizedBox(height: 16),
+                    const Text(
+                      "Ingredients:",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    ...meal.ingredients!.map((ingredient) {
+                      return Text('${ingredient['amount']} ${ingredient['unit']} ${ingredient['name']}');
+                    }).toList(),
                     const SizedBox(height: 16),
                     const Text(
                       "Steps:",
-                      style: TextStyle(fontSize: 16),
+                      style: TextStyle(fontWeight: FontWeight.bold),
                     ),
                     ...meal.steps!.asMap().entries.map((entry) {
                       final index = entry.key + 1;
                       final step = entry.value;
-                      return Text(
-                        '$index. $step',
-                        style: const TextStyle(fontSize: 16),
-                      );
+                      return Text('$index. $step');
                     }).toList(),
                   ],
                 ),
