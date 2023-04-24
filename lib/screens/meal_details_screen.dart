14e287bc2083ffcfb4095518865d6889e4619561
p-olszewski <przemo1297@gmail.com>
2023-04-24 21:56:35 +0200

    implement draggable scrollable sheet on the meal details screen

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -9,16 +9,16 @@ class MealDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            expandedHeight: 250,
-            pinned: true,
-            forceElevated: true,
-            flexibleSpace: FlexibleSpaceBar(
-              title: Text(meal.title),
-              background: Hero(
-                tag: 'meal',
+      body: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: Stack(
+          fit: StackFit.loose,
+          children: [
+            Hero(
+              tag: 'meal',
+              child: AspectRatio(
+                aspectRatio: 4 / 3,
                 child: Container(
                   decoration: BoxDecoration(
                     image: DecorationImage(
@@ -29,51 +29,76 @@ class MealDetailsScreen extends StatelessWidget {
                 ),
               ),
             ),
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    meal.title,
-                    style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            DraggableScrollableSheet(
+              initialChildSize: 0.7,
+              minChildSize: 0.7,
+              builder: (context, scrollController) {
+                return SafeArea(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    padding: const EdgeInsets.only(left: 28.0, top: 28, right: 28.0, bottom: 0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(40.0),
+                        topRight: Radius.circular(40.0),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          spreadRadius: 1,
+                          blurRadius: 6,
+                          offset: const Offset(1, 1),
+                        ),
+                      ],
+                    ),
+                    child: SingleChildScrollView(
+                      controller: scrollController,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            meal.title,
+                            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 16),
+                          Text(
+                              'Calories: ${meal.calories.round()} kcal\nProteins: ${meal.proteins.round()}g\nFats: ${meal.fats.round()}g\nCarbs: ${meal.carbs.round()}g\nReady in: ${meal.readyInMinutes} min\nServings: ${meal.servings}\nPrice per serving: ${meal.pricePerServing}\$'),
+                          const SizedBox(height: 16),
+                          const Text(
+                            "Dish types:",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          ...meal.dishTypes!.map((dishType) {
+                            return Text(dishType);
+                          }).toList(),
+                          const SizedBox(height: 16),
+                          const Text(
+                            "Ingredients:",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          ...meal.ingredients!.map((ingredient) {
+                            return Text('${ingredient['amount']} ${ingredient['unit']} ${ingredient['name']}');
+                          }).toList(),
+                          const SizedBox(height: 16),
+                          const Text(
+                            "Steps:",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          ...meal.steps!.asMap().entries.map((entry) {
+                            final index = entry.key + 1;
+                            final step = entry.value;
+                            return Text('$index. $step');
+                          }).toList(),
+                        ],
+                      ),
+                    ),
                   ),
-                  const SizedBox(height: 16),
-                  Text(
-                      'Calories: ${meal.calories.round()} kcal\nProteins: ${meal.proteins.round()}g\nFats: ${meal.fats.round()}g\nCarbs: ${meal.carbs.round()}g\nReady in: ${meal.readyInMinutes} min\nServings: ${meal.servings}\nPrice per serving: ${meal.pricePerServing}\$'),
-                  const SizedBox(height: 16),
-                  const Text(
-                    "Dish types:",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  ...meal.dishTypes!.map((dishType) {
-                    return Text(dishType);
-                  }).toList(),
-                  const SizedBox(height: 16),
-                  const Text(
-                    "Ingredients:",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  ...meal.ingredients!.map((ingredient) {
-                    return Text('${ingredient['amount']} ${ingredient['unit']} ${ingredient['name']}');
-                  }).toList(),
-                  const SizedBox(height: 16),
-                  const Text(
-                    "Steps:",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  ...meal.steps!.asMap().entries.map((entry) {
-                    final index = entry.key + 1;
-                    final step = entry.value;
-                    return Text('$index. $step');
-                  }).toList(),
-                ],
-              ),
+                );
+              },
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
