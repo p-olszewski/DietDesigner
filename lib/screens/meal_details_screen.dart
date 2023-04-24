e6fad983f73bcf6d938192d3bc1587e3d0722f1a
p-olszewski <przemo1297@gmail.com>
2023-04-24 10:59:08 +0200

    scrollable image on the meal details screen

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -12,67 +12,65 @@ class MealDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Meal details'),
       ),
-      body: Column(
-        children: [
-          Hero(
-            tag: 'meal',
-            child: Container(
-              width: double.infinity,
-              height: 200,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(meal.image),
-                  fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Hero(
+              tag: 'meal',
+              child: Container(
+                width: double.infinity,
+                height: 300,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(meal.image),
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      meal.title,
-                      style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 16),
-                    Text(
-                        'Calories: ${meal.calories.round()} kcal\nProteins: ${meal.proteins.round()}g\nFats: ${meal.fats.round()}g\nCarbs: ${meal.carbs.round()}g\nReady in: ${meal.readyInMinutes} min\nServings: ${meal.servings}\nPrice per serving: ${meal.pricePerServing}\$'),
-                    const SizedBox(height: 16),
-                    const Text(
-                      "Dish types:",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    ...meal.dishTypes!.map((dishType) {
-                      return Text(dishType);
-                    }).toList(),
-                    const SizedBox(height: 16),
-                    const Text(
-                      "Ingredients:",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    ...meal.ingredients!.map((ingredient) {
-                      return Text('${ingredient['amount']} ${ingredient['unit']} ${ingredient['name']}');
-                    }).toList(),
-                    const SizedBox(height: 16),
-                    const Text(
-                      "Steps:",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    ...meal.steps!.asMap().entries.map((entry) {
-                      final index = entry.key + 1;
-                      final step = entry.value;
-                      return Text('$index. $step');
-                    }).toList(),
-                  ],
-                ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    meal.title,
+                    style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                      'Calories: ${meal.calories.round()} kcal\nProteins: ${meal.proteins.round()}g\nFats: ${meal.fats.round()}g\nCarbs: ${meal.carbs.round()}g\nReady in: ${meal.readyInMinutes} min\nServings: ${meal.servings}\nPrice per serving: ${meal.pricePerServing}\$'),
+                  const SizedBox(height: 16),
+                  const Text(
+                    "Dish types:",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  ...meal.dishTypes!.map((dishType) {
+                    return Text(dishType);
+                  }).toList(),
+                  const SizedBox(height: 16),
+                  const Text(
+                    "Ingredients:",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  ...meal.ingredients!.map((ingredient) {
+                    return Text('${ingredient['amount']} ${ingredient['unit']} ${ingredient['name']}');
+                  }).toList(),
+                  const SizedBox(height: 16),
+                  const Text(
+                    "Steps:",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  ...meal.steps!.asMap().entries.map((entry) {
+                    final index = entry.key + 1;
+                    final step = entry.value;
+                    return Text('$index. $step');
+                  }).toList(),
+                ],
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
