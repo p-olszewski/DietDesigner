fd8fd33b98e52d9a3272439e3c407cf9aa900791
p-olszewski <przemo1297@gmail.com>
2023-04-24 09:03:39 +0200

    add meal details screen to routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,9 +1,16 @@
+import 'package:diet_designer/models/meal.dart';
 import 'package:diet_designer/screens/calculator_screen.dart';
 import 'package:diet_designer/screens/home_screen.dart';
 import 'package:diet_designer/screens/login_screen.dart';
+import 'package:diet_designer/screens/meal_details_screen.dart';
+import 'package:flutter/material.dart';
 
 var appRoutes = {
   '/home': (context) => const HomeScreen(),
   '/login': (context) => const LoginScreen(),
   '/calculator': (context) => const CalculatorScreen(),
+  '/meal_details': (context) {
+    final Meal meal = ModalRoute.of(context)!.settings.arguments as Meal;
+    return MealDetailsScreen(meal: meal);
+  },
 };
