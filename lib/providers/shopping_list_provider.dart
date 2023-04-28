b433208168d42e79b7d05cc737e6291862f9ef31
p-olszewski <przemo1297@gmail.com>
2023-04-28 23:44:40 +0200

    add item counter to the shopping list provider

--- lib/providers/shopping_list_provider.dart
+++ lib/providers/shopping_list_provider.dart
@@ -1,11 +1,14 @@
+import 'package:diet_designer/services/firestore_service.dart';
 import 'package:flutter/material.dart';
 
 class ShoppingListProvider with ChangeNotifier {
   String _listId = '';
   String _listTitle = '';
+  int _itemsCounter = 0;
 
   String get listId => _listId;
   String get listTitle => _listTitle;
+  int get itemsCounter => _itemsCounter;
 
   void setListId(String listId) {
     _listId = listId;
@@ -14,6 +17,11 @@ class ShoppingListProvider with ChangeNotifier {
 
   void setListTitle(String listTitle) {
     _listTitle = listTitle;
+    notifyListeners();
+  }
+
+  void countItems(String listId) async {
+    _itemsCounter = await getShoppingListElementsCount(listId);
     notifyListeners();
   }
 }
