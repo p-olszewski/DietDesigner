9743b0e16a647d470d6d80732bf8598a5a47b780
p-olszewski <przemo1297@gmail.com>
2023-05-17 23:49:00 +0200

    adjust shopping list pdf

--- lib/services/pdf_service.dart
+++ lib/services/pdf_service.dart
@@ -78,62 +78,105 @@ class PDFService {
         ),
       );
     }
-
-    final outputDir = await getExternalStorageDirectory();
-    if (outputDir == null) return;
-    final outputFile = File('${outputDir.path}/nutrition_plan_${nutritionPlan.date}.pdf');
-    await outputFile.writeAsBytes(await pdf.save());
-    await OpenFile.open(outputFile.path);
+    saveAndOpenFile(pdf, 'nutrition_plan_${formatDate(nutritionPlan.date)}');
   }
 
   static Future<void> generatePDFForShoppingList(String listName, String listId) async {
     final pdf = pw.Document();
     final products = await getShoppingListElements(listId);
+    const productsPerPage = 60;
 
-    pdf.addPage(
-      pw.Page(
-        pageFormat: PdfPageFormat.a4,
-        build: (context) => pw.Column(
-          crossAxisAlignment: pw.CrossAxisAlignment.start,
+    for (var pageIndex = 0; pageIndex < products.length; pageIndex += productsPerPage) {
+      final pageProducts = products.skip(pageIndex).take(productsPerPage).toList();
+      final rows = <pw.Widget>[];
+
+      for (var i = 0; i < pageProducts.length; i += 2) {
+        final productRow = pw.Row(
           children: [
-            pw.Row(
-              mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
-              children: [
-                pw.Text(
-                  'DietDesigner',
-                  style: pw.TextStyle(
-                    fontSize: 16,
-                    fontWeight: pw.FontWeight.bold,
-                  ),
-                ),
-                pw.Text(
-                  listName,
-                  style: pw.TextStyle(
-                    fontSize: 16,
-                    fontWeight: pw.FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-            pw.SizedBox(height: 60),
-            pw.Text(
-              'Ingredients:',
-              style: pw.TextStyle(fontSize: 14, fontWeight: pw.FontWeight.bold),
-            ),
-            pw.SizedBox(height: 5),
-            pw.Text(
-              products.join(' '),
-              style: const pw.TextStyle(fontSize: 12),
-            ),
+            _shoppingListItem(pageProducts, i),
           ],
+        );
+        if (i + 1 < pageProducts.length) {
+          productRow.children.add(
+            _shoppingListItem(pageProducts, i + 1),
+          );
+        }
+
+        rows.add(productRow);
+        rows.add(pw.SizedBox(height: 5));
+      }
+
+      pdf.addPage(
+        pw.Page(
+          pageFormat: PdfPageFormat.a4,
+          build: (context) => pw.Column(
+            crossAxisAlignment: pw.CrossAxisAlignment.start,
+            children: [
+              pw.Row(
+                mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                children: [
+                  pw.Text(
+                    'DietDesigner',
+                    style: pw.TextStyle(
+                      fontSize: 16,
+                      fontWeight: pw.FontWeight.bold,
+                    ),
+                  ),
+                  pw.Text(
+                    '$listName (${products.length} pcs.)',
+                    style: pw.TextStyle(
+                      fontSize: 16,
+                      fontWeight: pw.FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              pw.SizedBox(height: 40),
+              pw.SizedBox(height: 5),
+              pw.Column(
+                children: rows,
+              ),
+            ],
+          ),
         ),
+      );
+    }
+    saveAndOpenFile(pdf, 'shopping_list_$listName');
+  }
+
+  static pw.Expanded _shoppingListItem(List<String> pageProducts, int i) {
+    return pw.Expanded(
+      child: pw.Row(
+        mainAxisAlignment: pw.MainAxisAlignment.start,
+        children: [
+          pw.Container(
+            width: 10,
+            height: 10,
+            decoration: const pw.BoxDecoration(
+              border: pw.Border(
+                top: pw.BorderSide(width: 1),
+                right: pw.BorderSide(width: 1),
+                bottom: pw.BorderSide(width: 1),
+                left: pw.BorderSide(width: 1),
+              ),
+              borderRadius: pw.BorderRadius.all(pw.Radius.circular(2)),
+            ),
+          ),
+          pw.SizedBox(width: 10),
+          pw.Text(
+            pageProducts[i],
+            style: const pw.TextStyle(fontSize: 14),
+          ),
+        ],
       ),
     );
+  }
 
+  static saveAndOpenFile(pdf, fileName) async {
     final outputDir = await getExternalStorageDirectory();
     if (outputDir == null) return;
-    final outputFile = File('${outputDir.path}/shopping_list_$listName.pdf');
-    outputFile.writeAsBytes(await pdf.save());
-    OpenFile.open(outputFile.path);
+    final outputFile = File('${outputDir.path}/$fileName.pdf');
+    await outputFile.writeAsBytes(await pdf.save());
+    await OpenFile.open(outputFile.path);
   }
 }
