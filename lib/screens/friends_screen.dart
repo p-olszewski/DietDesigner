6d60c8884b8e8adc18eef5c8f3f3f3208c80dc1d
p-olszewski <przemo1297@gmail.com>
2023-05-18 21:21:59 +0200

    list container adjustment

--- lib/screens/friends_screen.dart
+++ lib/screens/friends_screen.dart
@@ -39,7 +39,7 @@ class _FriendsScreenState extends State<FriendsScreen> {
           height: screenHeight,
           child: SingleChildScrollView(
             child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 20.0),
+              padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 10.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -64,7 +64,7 @@ class _FriendsScreenState extends State<FriendsScreen> {
                       ],
                     ),
                   ),
-                  const SizedBox(height: 20),
+                  const SizedBox(height: 10),
                   FutureBuilder<List<User>>(
                     future: _friends,
                     builder: (context, snapshot) {
@@ -115,7 +115,7 @@ class _FriendsScreenState extends State<FriendsScreen> {
       margin: const EdgeInsets.symmetric(vertical: 5),
       decoration: BoxDecoration(
         color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
+        borderRadius: BorderRadius.circular(12),
         boxShadow: [
           BoxShadow(
             color: Colors.grey.withOpacity(0.5),
--- lib/widgets/shopping_list_tab.dart
+++ lib/widgets/shopping_list_tab.dart
@@ -57,6 +57,7 @@ class _ShoppingListTabState extends State<ShoppingListTab> {
               child: SingleChildScrollView(
                 child: Column(
                   children: [
+                    const SizedBox(height: 10),
                     _isLoading
                         ? const Center(child: CircularProgressIndicator())
                         : StreamBuilder(
@@ -80,44 +81,61 @@ class _ShoppingListTabState extends State<ShoppingListTab> {
                                     itemCount: snapshot.data!.docs.length,
                                     itemBuilder: (context, index) {
                                       var doc = snapshot.data!.docs[index];
-                                      return ListTile(
-                                        title: Text(doc['title']),
-                                        subtitle: Text('Items: ${doc['itemsCounter']}'),
-                                        trailing: const Icon(Icons.arrow_forward),
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.circular(10),
-                                        ),
-                                        onTap: () {
-                                          if (!mounted) return;
-                                          context.read<ShoppingListProvider>().setListId(doc.reference.id);
-                                          context.read<ShoppingListProvider>().setListTitle(doc['title']);
-                                          context.read<ShoppingListProvider>().countItems(doc.reference.id);
-                                          Navigator.pushNamed(context, '/shopping_list_details');
-                                        },
-                                        onLongPress: () {
-                                          showDialog(
-                                            context: context,
-                                            builder: (context) => AlertDialog(
-                                              title: const Text('Are you sure?'),
-                                              content: const Text('Do you want to delete this shopping list?'),
-                                              actions: [
-                                                TextButton(
-                                                  onPressed: () {
-                                                    Navigator.of(context).pop();
-                                                  },
-                                                  child: const Text('No'),
-                                                ),
-                                                FilledButton(
-                                                  onPressed: () {
-                                                    deleteShoppingList(doc.reference.id);
-                                                    Navigator.of(context).pop();
-                                                  },
-                                                  child: const Text('Yes'),
-                                                )
-                                              ],
+                                      return Container(
+                                        margin: const EdgeInsets.symmetric(vertical: 5),
+                                        decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius: BorderRadius.circular(12),
+                                          boxShadow: [
+                                            BoxShadow(
+                                              color: Colors.grey.withOpacity(0.5),
+                                              blurRadius: 5,
+                                              offset: const Offset(0, 2),
                                             ),
-                                          );
-                                        },
+                                          ],
+                                        ),
+                                        child: ListTile(
+                                          title: Text(
+                                            doc['title'],
+                                            style: Theme.of(context).textTheme.titleMedium!.copyWith(fontWeight: FontWeight.bold),
+                                          ),
+                                          subtitle: Text('Items: ${doc['itemsCounter']}'),
+                                          trailing: const Icon(Icons.chevron_right),
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(10),
+                                          ),
+                                          onTap: () {
+                                            if (!mounted) return;
+                                            context.read<ShoppingListProvider>().setListId(doc.reference.id);
+                                            context.read<ShoppingListProvider>().setListTitle(doc['title']);
+                                            context.read<ShoppingListProvider>().countItems(doc.reference.id);
+                                            Navigator.pushNamed(context, '/shopping_list_details');
+                                          },
+                                          onLongPress: () {
+                                            showDialog(
+                                              context: context,
+                                              builder: (context) => AlertDialog(
+                                                title: const Text('Are you sure?'),
+                                                content: const Text('Do you want to delete this shopping list?'),
+                                                actions: [
+                                                  TextButton(
+                                                    onPressed: () {
+                                                      Navigator.of(context).pop();
+                                                    },
+                                                    child: const Text('No'),
+                                                  ),
+                                                  FilledButton(
+                                                    onPressed: () {
+                                                      deleteShoppingList(doc.reference.id);
+                                                      Navigator.of(context).pop();
+                                                    },
+                                                    child: const Text('Yes'),
+                                                  )
+                                                ],
+                                              ),
+                                            );
+                                          },
+                                        ),
                                       );
                                     },
                                   );
